ee694bcde4e8498e92c70397d09374dbf3945e2f
ercusz <eurokungz.xkz@gmail.com>
2022-01-08 07:53:29 +0700

    implement factory method

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ value_list = [
             'password': 'hello' 
         }
     ]
-value_str = "{ 'email': 'test@example.com', 'password': 'test', 'user_info': { 'name': 'Test user', 'email': 'test@example.com', 'list': [ (1, 2, 3), 12.5 ] } }"
+value_str = "{ 'email': 'test@example.com', 'password': 'test', 'user_info': { 'name': 'Test user', 'email': 'test@example.com', 'list': [ (1, 2, '3'), 12.5 ] } }"
 value_str2 = "Hello World!"
 value_bytes = "{ 'email': 'test@example.com', 'password': 'test', 'user_info': { 'name': 'Test user', 'email': 'test@example.com' } }"
 value_bytes2 = b"hello world"
@@ -54,7 +54,6 @@ value_tuple = (
     )
 value_int = 123456
 value_float = 12.345
-
 value_custom = {
     "email": "test@example.com",
     "password": "test",
@@ -94,79 +93,114 @@ value_custom = {
     )
 }
 
-def type_decision(data):
+
+def data_transform_factory(data):
+    obj = None
     if isinstance(data, list):
-        return transform_list_data(data)
+        obj = ListDataTransform(data)
     elif isinstance(data, str):
-        return string_to_dict(data)
+        obj = StringDataTransform(data)
     elif isinstance(data, bytes):
-        return string_to_dict(data.decode())
+        obj = StringDataTransform(data.decode())
     elif isinstance(data, dict):
-        return transform_dict_data(data)
+        obj = DictDataTransform(data)
     elif isinstance(data, tuple):
-        return transform_tuple_data(data)
+        obj = TupleDataTransform(data)
     elif isinstance(data, int):
-        return data
+        obj = data
     elif isinstance(data, float):
-        return data
+        obj = data
     else:
-        return "Invalid data type"
-
-def transform_dict_data(data):
-    new_dict = {}
-    for k, v in data.items():
-        if isinstance(v, dict):
-            v = transform_dict_data(v)
-        if isinstance(v, list):
-            new_dict[k] = transform_list_data(v)
-        if isinstance(v, tuple):
-            new_dict[k] = transform_tuple_data(v)
-        
-        if not k == "password" and not isinstance(v, (list,tuple)):
-            new_dict[k] = v
-        if k == "email":
-            new_dict[k] = "redacted"
-
-    return new_dict
-
-def transform_list_data(data):
-    new_list = []
-    for v in data:
-        if isinstance(v, list):
-            new_list.append(transform_list_data(v))
-        elif isinstance(v, dict):
-            new_list.append(transform_dict_data(v))
-        elif isinstance(v, tuple):
-            new_list.append(transform_tuple_data(v))
-        else:
-            new_list.append(v)
-
-    return new_list
-
-def transform_tuple_data(data):
-    new_tuple = []
-    for v in data:
-        if isinstance(v, tuple):
-            new_tuple.append(transform_tuple_data(v))
-        elif isinstance(v, dict):
-            new_tuple.append(transform_dict_data(v))
-        elif isinstance(v, list):
-            new_tuple.append(transform_list_data(v))
-        else:
-            new_tuple.append(v)
+        raise ValueError('Invalid data!')
     
-    return tuple(new_tuple)
+    return obj
 
-def string_to_dict(data):
-    try:
-        data = eval(data)
-        return transform_dict_data(data)
-    except SyntaxError:
-        print('Syntax Error')
-        return data
+
+def data_transform(data):
+    obj = data_transform_factory(data)
+    if hasattr(obj, 'transform'):
+        return obj.transform()
+    else:
+        return obj
+
+
+class DictDataTransform():
+    def __init__(self, data):
+        self._data = data
+    
+    def transform(self):
+        new_dict = {}
+        for k, v in self._data.items():
+            if isinstance(v, dict):
+                self._data = v
+                v = self.transform()
+            if isinstance(v, list):
+                new_dict[k] = ListDataTransform(v).transform()
+            if isinstance(v, tuple):
+                new_dict[k] = TupleDataTransform(v).transform()
+            
+            if not k == "password" and not isinstance(v, (list,tuple)):
+                new_dict[k] = v
+            if k == "email":
+                new_dict[k] = "redacted"
+
+        return new_dict
+
+
+class ListDataTransform():
+    def __init__(self, data):
+        self._data = data
+
+    def transform(self):
+        new_list = []
+        for v in self._data:
+            if isinstance(v, list):
+                self._data = v
+                new_list.append(self.transform())
+            elif isinstance(v, dict):
+                new_list.append(DictDataTransform(v).transform())
+            elif isinstance(v, tuple):
+                new_list.append(TupleDataTransform(v).transform())
+            else:
+                new_list.append(v)
+
+        return new_list
+
+
+class TupleDataTransform():
+    def __init__(self, data):
+        self._data = data
+
+    def transform(self):
+        new_tuple = []
+        for v in self._data:
+            if isinstance(v, tuple):
+                self._data = v
+                new_tuple.append(self.transform())
+            elif isinstance(v, dict):
+                new_tuple.append(DictDataTransform(v).transform())
+            elif isinstance(v, list):
+                new_tuple.append(ListDataTransform(v).transform())
+            else:
+                new_tuple.append(v)
+    
+        return tuple(new_tuple)
+
+
+class StringDataTransform():
+    def __init__(self, data):
+        self._data = data
+
+    def transform(self):
+        try:
+            self._data = eval(self._data)
+            return DictDataTransform(self._data).transform()
+        except SyntaxError:
+            return self._data
+
 
 def main():
-    print(type_decision(value))
+    print(data_transform(value))
 
 
 if __name__ == '__main__':
